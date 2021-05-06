3b05a932b0945cd60d0a3e24717f764150053d54
devil1321 <s.dominik1321@gmail.com>
2021-05-06 14:37:53 +0200

    finished portrait only small landscape to fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 const navBtn = document.querySelector('.nav__btn')
 const navList = document.querySelector('.nav__list')
 const navItems = document.querySelectorAll('.nav__item')
-
+const overlay = document.querySelector('.overlay')
 navItems.forEach(item=>item.addEventListener('mouseenter',(e)=>{
     navItems.forEach(item => item.classList.remove('out'))
     e.target.classList.add('hover')
@@ -19,18 +19,26 @@ const handleNav = () =>{
         navBtn.classList.add('open')
         navList.style.width = "80vw"
         navList.style.height = "30vh"
+        overlay.style.opacity = '1'
+        overlay.style.visibility = 'visible'
+        if(window.innerWidth < 768){
+            navList.style.height = "45vh"
+        }
+
         time = 0;
         navItems.forEach(item=>{
             setTimeout(()=>{
                 item.style.opacity = 1
-            },time+=100)
+            },time+=120)
         })
     }else{
         navBtn.classList.remove('open')
+        navItems.forEach(item=> item.style.opacity = 0)
         navList.style.width = "0vw"
         navList.style.height = "0vh"
-        time = 0;
-        navItems.forEach(item=> item.style.opacity = 0)
+        overlay.style.opacity = '0'
+        overlay.style.visibility = 'hidden'
+
     }
 }
 navBtn.addEventListener('click',handleNav)

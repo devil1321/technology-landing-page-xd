394566a28735b62427ffe68a54c8511eeee21701
devil1321 <s.dominik1321@gmail.com>
2021-05-06 12:23:25 +0200

    updated nav on medium

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+const navBtn = document.querySelector('.nav__btn')
+const navList = document.querySelector('.nav__list')
 const navItems = document.querySelectorAll('.nav__item')
 
 navItems.forEach(item=>item.addEventListener('mouseenter',(e)=>{
@@ -10,3 +12,25 @@ navItems.forEach(item=>item.addEventListener('mouseleave',(e)=>{
         item.classList.add('out')
     })
 }))
+
+
+const handleNav = () =>{
+    if(!navBtn.classList.contains('open')){
+        navBtn.classList.add('open')
+        navList.style.width = "80vw"
+        navList.style.height = "30vh"
+        time = 0;
+        navItems.forEach(item=>{
+            setTimeout(()=>{
+                item.style.opacity = 1
+            },time+=100)
+        })
+    }else{
+        navBtn.classList.remove('open')
+        navList.style.width = "0vw"
+        navList.style.height = "0vh"
+        time = 0;
+        navItems.forEach(item=> item.style.opacity = 0)
+    }
+}
+navBtn.addEventListener('click',handleNav)
